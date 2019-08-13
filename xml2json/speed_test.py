b3366235caad995c25f9b5803c2645adad176d57
David Bruce Borenstein <david@appliednonprofitresearch.com>
2019-08-13 11:21:00 -0400

    Added a speed test

--- xml2json/speed_test.py
+++ xml2json/speed_test.py
@@ -12,7 +12,8 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 BASEPATH: str = os.path.dirname(os.path.abspath(__file__))
 
 xml_dir: str = os.path.join(BASEPATH, "..", "fixtures", "xml")
-json_dir: str = os.path.join(BASEPATH, "..", "fixtures", "json")
+json_dir: str = "/tmp/xml2json"
+os.makedirs(json_dir)
 
 translate: JsonTranslator = JsonTranslator()
 
@@ -21,7 +22,7 @@ chunk_time: float = 0.0
 # SO 2186525
 for i, xml_fn in enumerate(Path(xml_dir).glob("**/*.xml")):
     xml_fn = str(xml_fn)
-    json_relpath: str = "/".join(xml_fn.split("/")[-3:]).split(".")[0] + ".json"
+    json_relpath: str = "/".join(xml_fn.split("/")[-2:]).split(".")[0] + ".json"
     json_fn: str = os.path.join(json_dir, json_relpath)
     start = time.time()
     json_dirpath: str = "/".join(json_fn.split("/")[:-1])
